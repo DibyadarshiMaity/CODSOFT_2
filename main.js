60ff72aa9a64c7fbca56d3d256d5962b850a9f33
Dibyadarshi Maity <137876930+DibyadarshiMaity@users.noreply.github.com>
2023-11-04 13:41:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed=new Typed('#text',{
+strings:['Full Stack Developer','Coding Enthusiast','Software Developer'],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop:true
+}); 
+var typed=new Typed('#edu',{
+    strings:['Education','Qualifications'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    }); 
